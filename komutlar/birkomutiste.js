f6a91db5b9f3c4a541baf55f121820f08c64e61e
denememexd <43650840+denememexd@users.noreply.github.com>
2018-09-30 14:17:14 +0300

    Update birkomutiste.js

--- komutlar/birkomutiste.js
+++ komutlar/birkomutiste.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 exports.run = function(client, message, args) {
-  message.reply('**BlaxkLion Bot Bilgi**\n**---------------------** \nMerhaba Ben BLYBot \n**---------------------**\nEglence Icin Tasarlanmadım \n**---------------------**\nHizmet Icin Tasarlandim');
+  message.reply('**TFFamily Bot Bilgi**\n**---------------------** \nMerhaba Ben TFFamilyBot \n**---------------------**\nEglence Icin Tasarlanmadım \n**---------------------**\nHizmet Icin Tasarlandim');
 };
 
 exports.conf = {
